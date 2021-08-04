a03eb3eae9d21a270de7078111238bcfffd26d84
Peter Chacko <workpraytoday@gmail.com>
2021-08-04 06:05:04 +0000

    Got playback and skip working

--- public/js/music.js
+++ public/js/music.js
@@ -1,6 +1,8 @@
 const SPOTIFY_CLIENT_ID = "b213515c9c994acea2b15b7791d5afb4";
 const SPOTIFY_SCOPES = 'user-read-private user-read-email user-modify-playback-state user-read-currently-playing user-read-playback-state';
 const spotifyHeader = {}
+const POLL_TIME = 3000;
+let race_lock = false;
 
 function renderSpotifyLogin() {
     // Clear container
@@ -28,11 +30,12 @@ function updateSpotifyHeaders(accessToken) {
 
 function displaySpotifyController() {
     const spotifyContainer = document.querySelector("#spotify");
-    fetch(`https://api.spotify.com/v1/me/player/currently-playing`, { headers: spotifyHeader })
-        .then(response => response.json())
-        .then(data => data.item)
-        .then(song => {
+    getCurrentlyPlayingSpotify()
+        .then(data => {
+            const [isPlaying, song] = data;
+            //console.log(song);
             const albumImage = song.album.images[2];
+            setTimeout(pollForChanges, POLL_TIME);
             spotifyContainer.innerHTML = `<div id="spotifyControllerContainer" class="card">
                             <div class="card-content">
                                 <div id="spotifyController" class="media">
@@ -47,13 +50,16 @@ function displaySpotifyController() {
                                     <div class="media-content">
 
                                         <div id="spotifyPlaybackControlContainer">
-                                            <span class="material-icons outlined">
+
+                                            <span onclick="skipPrev()"  class="material-icons outlined">
                                                 skip_previous
                                             </span>
-                                            <span class="material-icons outlined">
-                                                play_arrow
+                                            
+                                            <span onclick="togglePlayback()" id="spotifySongStatus" class="material-icons outlined">
+                                                ${isPlaying ? 'pause': 'play_arrow' }
+                                                
                                             </span>
-                                            <span class="material-icons outlined">
+                                            <span onclick="skipNext()" class="material-icons outlined">
                                                 skip_next
                                             </span>
                                         </div>
@@ -66,4 +72,60 @@ function displaySpotifyController() {
                     `;
         });
 
+}
+// Need lock for race-condition
+function pollForChanges() {
+    if (!race_lock) {
+        getCurrentlyPlayingSpotify()
+            .then(([isPlaying, song]) => {
+                updateSpotifyCard(isPlaying, song);
+            });
+    }
+
+    setTimeout(pollForChanges, POLL_TIME);
+}
+
+// Check if not playing anything
+const getCurrentlyPlayingSpotify = async () => {
+    const data = await fetch(`https://api.spotify.com/v1/me/player/currently-playing`, { headers: spotifyHeader }).then(response => response.json()).catch(err => console.log(err));
+    return [data.is_playing, data.item];
+}
+
+const togglePlayback = async () => {
+    const [isPlaying, song] = await getCurrentlyPlayingSpotify();
+    race_lock = true;
+    if (isPlaying) {
+        await fetch(`https://api.spotify.com/v1/me/player/pause`, { method: 'PUT', headers: spotifyHeader }).then(response => response.json()).catch(err => console.log(err));
+    } else {
+        await fetch(`https://api.spotify.com/v1/me/player/play`, { method: 'PUT', headers: spotifyHeader }).then(response => response.json()).catch(err => console.log(err));
+    }
+    document.querySelector("#spotifySongStatus").textContent = !isPlaying ? 'pause': 'play_arrow';
+    race_lock = false;
+}
+
+const skipNext = async () => {
+    const [oldIsPlaying, oldSong] = await getCurrentlyPlayingSpotify();
+    race_lock = true;
+    await fetch(`https://api.spotify.com/v1/me/player/next`, { method: 'POST', headers: spotifyHeader }).then(response => response.json()).catch(err => console.log(err));
+    const [newIsPlaying, newSong] = await getCurrentlyPlayingSpotify();
+    updateSpotifyCard(newIsPlaying, newSong);
+    race_lock = false;
+}
+
+const skipPrev = async () => {
+    const [oldIsPlaying, oldSong] = await getCurrentlyPlayingSpotify();
+    race_lock = true;
+    await fetch(`https://api.spotify.com/v1/me/player/previous`, { method: 'POST', headers: spotifyHeader }).then(response => response.json()).catch(err => console.log(err));
+    const [newIsPlaying, newSong] = await getCurrentlyPlayingSpotify();
+    updateSpotifyCard(newIsPlaying, newSong);
+    race_lock = false;
+}
+
+function updateSpotifyCard(isPlaying, song) {
+    document.querySelector("#spotifySongStatus").textContent = isPlaying ? 'pause': 'play_arrow';
+    document.querySelector("#spotifySongTitle").textContent = song.name;
+    document.querySelector("#spotifySongArtist").textContent = song.artists[0].name;
+    const albumImage = song.album.images[2];
+    document.querySelector("#spotifyCurrentSongImage").src = albumImage.url;
+    document.querySelector("#spotifyCurrentSongImage").alt = song.album.name;
 }
